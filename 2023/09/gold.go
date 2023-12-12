e5680887f16c9826390f1061d9250854b68c2da7
Railson Ferreira <railson.info@gmail.com>
2023-12-12 01:46:15 +0000

    feat(2023): Add 2023/09 silver+gold

---
+++ 2023/09/gold.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func main() {
+	bytes, err := os.ReadFile("input.txt")
+	check(err)
+	input := string(bytes)
+
+	sequences := formatInput(input)
+
+	sum := 0
+	for _, sequence := range sequences {
+		sum += findTheExtrapolateNumberBackwards(sequence)
+	}
+
+	fmt.Println(sum)
+
+}
+
+func formatInput(input string) (sequences [][]int) {
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		var sequence []int
+		for _, value := range strings.Split(line, " ") {
+			valueInt, _ := strconv.Atoi(value)
+			sequence = append(sequence, valueInt)
+		}
+
+		sequences = append(sequences, sequence)
+
+	}
+
+	return sequences
+}
+
+func getDiffSequence(sequence []int) (diffSequence []int) {
+	for index := range sequence {
+		if index == 0 {
+			continue
+		}
+		diffSequence = append(diffSequence, sequence[index]-sequence[index-1])
+	}
+	return diffSequence
+}
+
+func areAllZeros(sequence []int) bool {
+	for _, value := range sequence {
+		if value != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func findTheExtrapolateNumberBackwards(sequence []int) int {
+	diffSequence := getDiffSequence(sequence)
+	if areAllZeros(diffSequence) {
+		return sequence[0]
+	}
+	diffExtrapolateNumber := findTheExtrapolateNumberBackwards(diffSequence)
+
+	return sequence[0] - diffExtrapolateNumber
+}
